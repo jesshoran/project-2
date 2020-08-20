af41c5b0cc985a8965c6e87fd1c8fe7234f0a0ca
lakergirl702 <jesshoran87@gmail.com>
2020-08-20 10:10:42 -0700

    added server.js packages .env

---
+++ server.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+// dependencies
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+app.use(methodOverride('_method'));
+const mongoURI = process.env.MONGO_URI;
+
+// mongoose connection
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once('open', () => {
+    console.log("connected to mongo");
+})
+
+// Controller
+
+// const logsController = require('./controllers/logs.js');
+// app.use('/logs', logsController);
+
+
+// const crewController = require('./controllers/dacrew.js');
+// app.use('/crew', crewController);
+
+
+
+// Homepage
+
+app.get('/', (req, res)=> {
+    res.send("Welcome to the Homepage")    
+}) 
+
+// listen
+app.listen(PORT, () => {
+    console.log('listening on: ' + PORT);
+});
+
